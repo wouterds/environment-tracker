335b3d353b1a72e1b78716f3a1742425bc4740af
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-04-17 00:10:28 +0200

    :tada: Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+//@flow
+import ReactDOM from 'react-dom';
+import React from 'react';
+import type { Node } from 'react';
+import styles from 'styles/core.css';
+
+/**
+ * Render the component
+ *
+ * @returns {Node}
+ */
+const Index = (): Node => {
+  return (
+    <h1>Hello World</h1>
+  );
+};
+
+const root = document.getElementById('root');
+
+if (root === null) {
+  throw new Error('Could not find root element!');
+}
+
+root.className = styles.root;
+ReactDOM.render(<Index />, root);
