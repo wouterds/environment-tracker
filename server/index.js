ecbe8441b1d1cf6413b11658b044393c3b515baa
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-04-18 16:27:56 +0000

    :zap: Use ES6 in server as well and run with babel-node

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-const redis = require('redis');
+import redis from 'redis';
+import { promisify } from 'util';
+import WebSocket from 'ws';
+
+// Init Redis clients
 const client = redis.createClient({ host: 'redis' });
 const sub = redis.createClient({ host: 'redis' });
-const { promisify } = require('util');
-const getAsync = promisify(client.get).bind(client);
-const WebSocket = require('ws');
 
+// Get async from Redis proxy
+const getAsync = promisify(client.get).bind(client);
+
+// WebSocker server
 const wss = new WebSocket.Server({ port: 3000 });
+
+/**
+ * Broadcast to all clients
+ *
+ * @param {mixed} data
+ */
 wss.broadcast = (data) => {
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
@@ -14,42 +25,43 @@ wss.broadcast = (data) => {
   });
 };
 
-client.on('error', function (err) {
-  console.error(`Error (client): ${err}`);
-});
-
-sub.on('error', function (err) {
-  console.error(`Error (sub): ${err}`);
-});
-
 let sensors = {};
 sub.on('message', async (channel, message) => {
   let sensor = message;
   let value = await getAsync(`sensor:${sensor}`);
 
+  // If there's a value, parse as json
   if (typeof value !== 'undefined') {
     value = JSON.parse(value);
   }
 
+  // Doesn't exist yet?
   if (!sensors.hasOwnProperty(sensor)) {
     sensors[sensor] = value;
   }
 
+  // Don't trigger update if value unchanged
   if (JSON.stringify(sensors[sensor]) === JSON.stringify(value)) {
     return;
   }
 
+  // Cache
   sensors[sensor] = value;
 
+  // Broadcast
   wss.broadcast({ sensor, data: value });
 
-  console.log(`Value updated for ${sensor}`, value);
+  // Debugging output
+  console.log({ sensor, data: value });
 });
 
+// New connection
 wss.on('connection', (ws) => {
+  // Send all sensor data
   Object.entries(sensors).forEach(([sensor, data]) => {
     ws.send(JSON.stringify({ sensor, data }));
   });
 });
 
+// Subscribe to sensor publish event
 sub.subscribe('sensor');
