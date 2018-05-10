0ec02ada5195d681b4fddfb4d78422c9b46e0ac4
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-05-10 18:09:23 -0400

    :bug: Default active sensor should use location hash value

--- src/store/reducers/activeSensor.js
+++ src/store/reducers/activeSensor.js
@@ -1,8 +1,8 @@
 import { SET_ACTIVE_SENSOR } from 'store/actions/activeSensor';
 
-const EMPTY_ACTIVE_SENSOR = null;
+const DEFAULT_ACTIVE_SENSOR = location && location.hash ? location.hash.replace('#', '') : null;
 
-const activeSensor = (state = EMPTY_ACTIVE_SENSOR, action) => {
+const activeSensor = (state = DEFAULT_ACTIVE_SENSOR, action) => {
   switch (action.type) {
     case SET_ACTIVE_SENSOR:
       return action.sensor;
