f1d4f62a20924417944d790a3a3d168d52372d01
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-05-10 17:49:30 -0400

    :zap: Set active sensor if not set yet

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setSensors } from 'store/actions/sensors'
+import { getSensors } from 'store/selectors/sensors';
+import { getActiveSensor } from 'store/selectors/activeSensor';
+import { setSensors } from 'store/actions/sensors';
+import { setActiveSensor } from 'store/actions/activeSensor';
 
 const App = (WrappedComponent) => {
   class App extends Component {
+    /**
+     * Component will receive props
+     *
+     * @param {Object} nextProps
+     */
+    componentWillReceiveProps(nextProps) {
+      const { setActiveSensor } = this.props;
+
+      // No active sensor yet but we do have temperature sensor
+      if (nextProps.sensors.temperature && !nextProps.activeSensor) {
+        setActiveSensor('temperature');
+      }
+    }
+
     /**
      * Render
      *
@@ -13,6 +30,19 @@ const App = (WrappedComponent) => {
       return <WrappedComponent {...this.props} />;
     }
   }
+
+  /**
+   * Map state to props
+   *
+   * @param {Object} state
+   * @return {Object}
+   */
+  const mapStateToProps = (state) => {
+    return {
+      sensors: getSensors(state),
+      activeSensor: getActiveSensor(state),
+    };
+  };
 
   /**
    * Map dispatch to props
@@ -25,10 +55,13 @@ const App = (WrappedComponent) => {
       setSensors: (data) => {
         dispatch(setSensors(data));
       },
+      setActiveSensor: (data) => {
+        dispatch(setActiveSensor(data));
+      },
     };
   };
 
-  return connect(null, mapDispatchToProps)(App);
+  return connect(mapStateToProps, mapDispatchToProps)(App);
 };
 
 export default App;
