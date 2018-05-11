fa9b5e9c77a62f9bc539c7211b10ef2770dd8e4d
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-05-11 01:26:15 -0400

    :bug: Must parse as floats

--- src/store/parsers/charts.js
+++ src/store/parsers/charts.js
@@ -5,19 +5,19 @@ const parse = (raw) => {
     case 'bme280':
       data.temperature = raw.data
         .filter(item => item.type === 'temperature')
-        .map(item => item.value);
+        .map(item => parseFloat(item.value));
       data.humidity = raw.data
         .filter(item => item.type === 'humidity')
-        .map(item => item.value);
+        .map(item => parseFloat(item.value));
       data.pressure = raw.data
         .filter(item => item.type === 'pressure')
-        .map(item => item.value);
+        .map(item => parseFloat(item.value));
       break;
 
     case 'bh1750':
       data.light = raw.data
         .filter(item => item.type === 'light')
-        .map(item => item.value);
+        .map(item => parseFloat(item.value));
       break;
   }
 
