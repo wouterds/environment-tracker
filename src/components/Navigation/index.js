798ba2877d10c2801f6ffd6dfd05126a07cd0c9f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-06-20 00:27:07 +0200

    :wheelchair: More readable code

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -32,7 +32,7 @@ class Navigation extends Component<Props> {
   /**
    * Render navigation
    *
-   * @return {Node}
+   * @returns {Node}
    */
   render(): Node {
     const {
@@ -51,78 +51,136 @@ class Navigation extends Component<Props> {
     return (
       <div className={styles.container}>
         <div className={styles.left}>
-          <select
-            className={styles.mobileSelect}
-            defaultValue={activeSensor}
-            onChange={(event) => setActiveSensor(event.target.value)}
-            >
-            {temperature && (<option value="temperature">Temperature</option>)}
-            {humidity && (<option value="humidity">Humidity</option>)}
-            {pressure && (<option value="pressure">Pressure</option>)}
-            {co2 && (<option value="co2">CO2</option>)}
-            {light && (<option value="light">Light</option>)}
-          </select>
-
-          <ul>
-            {temperature && (
-              <li
-                className={cx(styles.item, activeSensor === 'temperature' ? styles.active : null)}
-                onClick={() => setActiveSensor('temperature')}
-                >
-                Temperature &middot; {formatTemperature(temperature.value)}
-                <span className={styles.legendUnit}>{temperature.unit}</span>
-              </li>
-            )}
-            {humidity && (
-              <li
-                className={cx(styles.item, activeSensor === 'humidity' ? styles.active : null)}
-                onClick={() => setActiveSensor('humidity')}
-                >
-                Humidity &middot; {formatHumidity(humidity.value)}
-                <span className={styles.legendUnit}>{humidity.unit}</span>
-              </li>
-            )}
-            {pressure && (
-              <li
-                className={cx(styles.item, activeSensor === 'pressure' ? styles.active : null)}
-                onClick={() => setActiveSensor('pressure')}
-                >
-                Pressure &middot; {formatPressure(pressure.value)}
-                <span className={styles.legendUnit}>{pressure.unit}</span>
-              </li>
-            )}
-            {co2 && (
-              <li
-                className={cx(styles.item, activeSensor === 'co2' ? styles.active : null)}
-                onClick={() => setActiveSensor('co2')}
-                >
-                CO2 &middot; {formatCo2(co2.value)}
-                <span className={styles.legendUnit}>{co2.unit}</span>
-              </li>
-            )}
-            {light && (
-              <li
-                className={cx(styles.item, activeSensor === 'light' ? styles.active : null)}
-                onClick={() => setActiveSensor('light')}
-                >
-                Light &middot; {formatLight(light.value)}
-                <span className={styles.legendUnit}>{light.unit}</span>
-              </li>
-            )}
-          </ul>
+          {this.renderMobileSensors}
+          {this.renderSensors}
         </div>
         <div className={styles.right}>
-          <ul>
-            {periods.map((period) => (
-              <li key={`period-item-${period}`}
-                className={cx(styles.item, activePeriod === period ? styles.active : null)}
-                onClick={() => setActivePeriod(period)}>
-                {period}
-              </li>
-            ))}
-          </ul>
+          {this.renderPeriods}
         </div>
       </div>
+    );
+  }
+
+  /**
+   * Render sensors
+   *
+   * @returns {Node}
+   */
+  get renderSensors(): Node {
+    const {
+      activeSensor,
+      temperature,
+      pressure,
+      humidity,
+      co2,
+      light,
+      setActiveSensor,
+    } = this.props;
+
+    return (
+      <ul>
+        {temperature && (
+          <li
+            className={cx(styles.item, activeSensor === 'temperature' ? styles.active : null)}
+            onClick={() => setActiveSensor('temperature')}
+            >
+            Temperature &middot; {formatTemperature(temperature.value)}
+            <span className={styles.legendUnit}>{temperature.unit}</span>
+          </li>
+        )}
+        {humidity && (
+          <li
+            className={cx(styles.item, activeSensor === 'humidity' ? styles.active : null)}
+            onClick={() => setActiveSensor('humidity')}
+            >
+            Humidity &middot; {formatHumidity(humidity.value)}
+            <span className={styles.legendUnit}>{humidity.unit}</span>
+          </li>
+        )}
+        {pressure && (
+          <li
+            className={cx(styles.item, activeSensor === 'pressure' ? styles.active : null)}
+            onClick={() => setActiveSensor('pressure')}
+            >
+            Pressure &middot; {formatPressure(pressure.value)}
+            <span className={styles.legendUnit}>{pressure.unit}</span>
+          </li>
+        )}
+        {co2 && (
+          <li
+            className={cx(styles.item, activeSensor === 'co2' ? styles.active : null)}
+            onClick={() => setActiveSensor('co2')}
+            >
+            CO2 &middot; {formatCo2(co2.value)}
+            <span className={styles.legendUnit}>{co2.unit}</span>
+          </li>
+        )}
+        {light && (
+          <li
+            className={cx(styles.item, activeSensor === 'light' ? styles.active : null)}
+            onClick={() => setActiveSensor('light')}
+            >
+            Light &middot; {formatLight(light.value)}
+            <span className={styles.legendUnit}>{light.unit}</span>
+          </li>
+        )}
+      </ul>
+    );
+  }
+
+  /**
+   * Render mobile sensors
+   *
+   * @returns {Node}
+   */
+  get renderMobileSensors(): Node {
+    const {
+      activeSensor,
+      temperature,
+      pressure,
+      humidity,
+      co2,
+      light,
+      setActiveSensor,
+    } = this.props;
+
+    return (
+      <select
+        className={styles.mobileSelect}
+        defaultValue={activeSensor}
+        onChange={(event) => setActiveSensor(event.target.value)}
+        >
+        {temperature && (<option value="temperature">Temperature</option>)}
+        {humidity && (<option value="humidity">Humidity</option>)}
+        {pressure && (<option value="pressure">Pressure</option>)}
+        {co2 && (<option value="co2">CO2</option>)}
+        {light && (<option value="light">Light</option>)}
+      </select>
+    );
+  }
+
+  /**
+   * Render periods
+   *
+   * @returns {Node}
+   */
+  get renderPeriods(): Node {
+    const {
+      activePeriod,
+      periods,
+      setActivePeriod,
+    } = this.props;
+
+    return (
+      <ul>
+        {periods.map((period) => (
+          <li key={`period-item-${period}`}
+            className={cx(styles.item, activePeriod === period ? styles.active : null)}
+            onClick={() => setActivePeriod(period)}>
+            {period}
+          </li>
+        ))}
+      </ul>
     );
   }
 }
