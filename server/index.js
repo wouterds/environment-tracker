15bfc332a02d6a72e4fc5af91bdd4795728501bd
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-06-20 00:59:15 +0000

    :hankey: Time periods WIP

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import redis from 'redis';
 import { promisify } from 'util';
 import WebSocket from 'ws';
 import Sequelize from 'sequelize';
+import uuid from 'uuid';
 
 // Init Sequelize client
 const sequelize = new Sequelize({
@@ -22,14 +23,17 @@ const getAsync = promisify(client.get).bind(client);
 // WebSocker server
 const wss = new WebSocket.Server({ port: 3000 });
 
+// Connected clients
+let connectedClients = {};
+
 /**
  * Broadcast to all clients
  *
  * @param {mixed} data
  */
-wss.broadcast = (data) => {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
+wss.broadcast = (period, data) => {
+  Object.values(connectedClients).forEach((client) => {
+    if ((period === null || client.period === period) && client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(data));
     }
   });
@@ -59,7 +63,7 @@ sub.on('message', async (channel, message) => {
   sensors[sensor] = value;
 
   // Broadcast
-  wss.broadcast({ type: 'sensor', sensor, data: value });
+  wss.broadcast(null, { type: 'sensor', sensor, data: value });
 
   // Debugging output
   console.log({ type: 'sensor', sensor, data: value });
@@ -67,13 +71,36 @@ sub.on('message', async (channel, message) => {
 
 // New connection
 wss.on('connection', (ws) => {
+  const id = uuid.v4();
+
+  ws.id = id;
+  connectedClients[id] = ws;
+
+  ws.on('message', (data) => clientSendMessage(ws, data));
+});
+
+// Connected closed
+wss.on('close', (ws) => {
+  delete connectedClients[ws.id];
+});
+
+// Client sent us a message
+const clientSendMessage = (ws, data) => {
+  console.log(`Message from ${ws.id}`, data);
+
+  const message = JSON.parse(data);
+
+  if (message.period) {
+    ws.period = message.period;
+  }
+
   // Send all sensor data
   Object.entries(sensors).forEach(([sensor, data]) => {
     ws.send(JSON.stringify({ type: 'sensor', sensor, data }));
 
     broadcastChartForSensor(sensor);
   });
-});
+};
 
 // Measurements model
 const Measurement = sequelize.define('measurement', {
@@ -94,19 +121,46 @@ const Measurement = sequelize.define('measurement', {
 });
 
 const broadcastChartForSensor = async (sensor) => {
-  let measurements = await sequelize.query(`
-    SELECT
-      type,
-      AVG(CAST(value AS DECIMAL)) AS value,
-      TO_TIMESTAMP(FLOOR((EXTRACT(EPOCH FROM "createdAt") / 600 )) * 600) AT TIME ZONE 'UTC' as time_interval
-    FROM measurements
-    WHERE sensor = '${sensor}' AND "createdAt" >= NOW() - '1 DAY'::INTERVAL
-    GROUP BY sensor, type, time_interval
-    ORDER BY time_interval ASC;
-  `, { type: Sequelize.QueryTypes.SELECT });
+  const periods = ['6H', '1D', '1W', '1M'];
+  for (let index = 0; index < periods.length; index++) {
+    const period = periods[index];
+    const dataPoints = 60;
+    let interval = '1 DAY';
+    let groupByMinutes = ((1 * 60) / dataPoints) * 60;
 
-  // Chart events
-  wss.broadcast({ type: 'chart', sensor, data: measurements });
+    switch (period) {
+      case '6H':
+        interval = '6 HOUR';
+        groupByMinutes = ((6 * 60) / dataPoints) * 60;
+        break;
+      case '1D':
+        interval = '1 DAY';
+        groupByMinutes = ((24 * 60) / dataPoints) * 60;
+        break;
+      case '1W':
+        interval = '1 WEEK';
+        groupByMinutes = ((7 * 24 * 60) / dataPoints) * 60;
+        break;
+      case '1M':
+        interval = '1 MONTH';
+        groupByMinutes = ((30 * 24 * 60) / dataPoints) * 60;
+        break;
+    }
+
+    const measurements = await sequelize.query(`
+      SELECT
+        type,
+        AVG(CAST(value AS DECIMAL)) AS value,
+        TO_TIMESTAMP(FLOOR((EXTRACT(EPOCH FROM "createdAt") / ${groupByMinutes} )) * ${groupByMinutes}) AT TIME ZONE 'UTC' as time_interval
+      FROM measurements
+      WHERE sensor = '${sensor}' AND "createdAt" >= NOW() - '${interval}'::INTERVAL
+      GROUP BY sensor, type, time_interval
+      ORDER BY time_interval ASC;
+    `, { type: Sequelize.QueryTypes.SELECT });
+
+    // Chart events
+    wss.broadcast(period, { type: 'chart', sensor, data: measurements });
+  }
 };
 
 // Calculate minute averages
