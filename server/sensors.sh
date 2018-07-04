3b93fc4d4eed1e49c47c6e224b7bd19840dfe4a0
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-07-04 16:49:47 +0000

    :sparkles: Auto start SGP30

--- server/sensors.sh
+++ server/sensors.sh
@@ -5,4 +5,5 @@
 ./sensors/tmp102.py > /dev/null 2>&1 &
 ./sensors/bh1750.py > /dev/null 2>&1 &
 ./sensors/ccs811.py > /dev/null 2>&1 &
+./sensors/sgp30.py > /dev/null 2>&1 &
 ./sensors/pir.py > /dev/null 2>&1 &
