d394ef7419e802c335831c298da06dd4545a64f8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-19 23:41:12 +0100

    :sparkles: Express http server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+import * as express from 'express';
+
+// Load .env
+config();
+
+// Create http server
+const app = express();
+
+// Routes
+app.get(
+  '/',
+  (_request: express.Request, response: express.Response): express.Response =>
+    response.sendStatus(200),
+);
+
+// Start http server
+app.listen(3000);
+
+/* tslint:disable */
+console.log('Application running 🚀!');
