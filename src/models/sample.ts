f165c5ff3894218edadf3d0ea15fa6b7e5400adc
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-19 21:25:16 +0100

    :card_file_box: No use for id

--- src/models/sample.ts
+++ src/models/sample.ts
@@ -2,7 +2,6 @@ import * as sequelize from 'sequelize';
 import db from '../services/database';
 
 export interface Definition {
-  id: string;
   sensorId: string;
   value: number;
 }
@@ -10,11 +9,6 @@ export interface Definition {
 export default db.define<Definition, Partial<Definition>>(
   'sample',
   {
-    id: {
-      primaryKey: true,
-      type: sequelize.UUID,
-      defaultValue: sequelize.UUIDV4,
-    },
     sensorId: { type: sequelize.UUID, allowNull: false },
     value: { type: sequelize.FLOAT(8, 2), allowNull: false },
   },
