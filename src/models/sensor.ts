cd799756caec33780300862bce69950e00c77357
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-19 23:08:11 +0100

    :card_file_box: Added & renamed some fields

--- src/models/sensor.ts
+++ src/models/sensor.ts
@@ -1,10 +1,25 @@
 import * as sequelize from 'sequelize';
 import db from '../services/database';
 
+export enum Sensor {
+  BME280,
+  SGP30,
+  BH1750,
+}
+
+export enum Type {
+  ILLUMINANCE,
+  HUMIDITY,
+  PRESSURE,
+  ECO2,
+  TEMPERATURE,
+}
+
 export interface Definition {
   id: string;
-  name: string;
-  type: string;
+  description: string;
+  sensor: Sensor;
+  type: Type;
   unit: string;
 }
 
@@ -16,15 +31,19 @@ export default db.define<Definition, Partial<Definition>>(
       type: sequelize.UUID,
       defaultValue: sequelize.UUIDV4,
     },
-    name: { type: sequelize.STRING(32), allowNull: false },
-    type: { type: sequelize.STRING(8), allowNull: false },
+    description: { type: sequelize.STRING(64), allowNull: false },
+    sensor: { type: sequelize.STRING(8), allowNull: false },
+    type: { type: sequelize.STRING(16), allowNull: false },
     unit: { type: sequelize.STRING(4), allowNull: false },
   },
   {
     indexes: [
       {
-        fields: ['name', 'type'],
+        fields: ['type'],
         unique: true,
+      },
+      {
+        fields: ['sensor'],
       },
     ],
   },
