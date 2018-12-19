bcc2a1a82f54eaa3e1c7c73d58b67dcc8549ab07
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-20 00:45:53 +0100

    :sparkles: Added SensorRepository.getAll

--- src/repositories/sensor.ts
+++ src/repositories/sensor.ts
@@ -1,5 +1,9 @@
 import SensorModel, { Definition, Type } from '../models/sensor';
 
+export const getAll = async (): Promise<Definition[]> => {
+  return SensorModel.findAll();
+};
+
 export const getByType = async (type: Type): Promise<Definition | null> => {
   return SensorModel.findOne({ where: { type } });
 };
