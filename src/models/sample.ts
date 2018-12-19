775352405c8c06e663fe63077e2e328ef7dae228
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-19 19:45:57 +0100

    :card_file_box: Added sample model

---
+++ src/models/sample.ts
@@ -0,0 +1,28 @@
+import * as sequelize from 'sequelize';
+import db from '../services/database';
+
+export interface Definition {
+  id: string;
+  sensorId: string;
+  value: number;
+}
+
+export default db.define<Definition, Partial<Definition>>(
+  'sample',
+  {
+    id: {
+      primaryKey: true,
+      type: sequelize.UUID,
+      defaultValue: sequelize.UUIDV4,
+    },
+    sensorId: { type: sequelize.UUID, allowNull: false },
+    value: { type: sequelize.FLOAT(8, 2), allowNull: false },
+  },
+  {
+    indexes: [
+      {
+        fields: ['sensorId'],
+      },
+    ],
+  },
+);
