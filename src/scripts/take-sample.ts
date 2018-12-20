cd8f9aaebab3da9981fdf54e01130bf247d121e9
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-20 09:42:12 +0100

    :sparkles: Save average out samples as one sample to db

--- src/scripts/take-sample.ts
+++ src/scripts/take-sample.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { addSeconds, isFuture } from 'date-fns';
 import { config } from 'dotenv';
+import { meanBy } from 'lodash';
+import { Type } from '../models/sensor';
+import * as SampleRepository from '../repositories/sample';
+import * as SensorRepository from '../repositories/sensor';
 
 config();
 
@@ -30,7 +34,30 @@ interface ApiResponse {
     }
   }
 
-  console.log({ samples });
+  for (const sensor of await SensorRepository.getAll()) {
+    switch (sensor.type) {
+      case Type.TEMPERATURE:
+        const averageTemperature = meanBy(samples, 'temperature');
+        await SampleRepository.add(sensor.id, averageTemperature);
+        break;
+      case Type.HUMIDITY:
+        const averageHumidity = meanBy(samples, 'humidity');
+        await SampleRepository.add(sensor.id, averageHumidity);
+        break;
+      case Type.PRESSURE:
+        const averagePressure = meanBy(samples, 'pressure');
+        await SampleRepository.add(sensor.id, averagePressure);
+        break;
+      case Type.ECO2:
+        const averageEco2 = meanBy(samples, 'eco2');
+        await SampleRepository.add(sensor.id, averageEco2);
+        break;
+      case Type.ILLUMINANCE:
+        const averageIlluminance = meanBy(samples, 'illuminance');
+        await SampleRepository.add(sensor.id, averageIlluminance);
+        break;
+    }
+  }
 
   process.exit();
 })();
