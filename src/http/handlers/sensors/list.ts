f5b045447b1a5799129bd8ed9766239f539424d9
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-22 01:22:37 +0100

    :wheelchair: Return 204 when no content

--- src/http/handlers/sensors/list.ts
+++ src/http/handlers/sensors/list.ts
@@ -4,5 +4,9 @@ import * as SensorRepository from '../../../repositories/sensor';
 export default async (_req: Request, res: Response): Promise<Response> => {
   const sensors = await SensorRepository.getAll();
 
+  if (sensors.length === 0) {
+    return res.status(204).json(sensors);
+  }
+
   return res.status(200).json(sensors);
 };
