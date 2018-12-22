d8077f60e793f2ac78582c491bb212bd9ede3b19
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-22 01:02:59 +0100

    :card_file_box: Order by createdAt descending

--- src/repositories/sensor.ts
+++ src/repositories/sensor.ts
@@ -1,7 +1,9 @@
 import SensorModel, { Definition, Type } from '../models/sensor';
 
 export const getAll = async (): Promise<Definition[]> => {
-  return SensorModel.findAll();
+  return SensorModel.findAll({
+    order: [['createdAt', 'DESC']],
+  });
 };
 
 export const getByType = async (type: Type): Promise<Definition | null> => {
