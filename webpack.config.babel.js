561110d0d86f1b916de6d44c349bba2fcbdb8078
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-24 12:48:25 +0100

    :construction_worker: Added css & file support to webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -23,6 +23,59 @@ export default (env, { mode }) => {
           exclude: /node_modules/,
           loader: 'babel-loader',
         },
+        {
+          test: /\.css$/,
+          loaders: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: true,
+                localIdentName: isProduction ? '[hash:7]' : '[name]-[local]-[hash:7]',
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: (loader) => [
+                  require('postcss-pseudoelements')(),
+                  require('postcss-import')({
+                    path: 'src/styles',
+                  }),
+                  require('postcss-mixins')(),
+                  require('postcss-preset-env')({
+                    features: {
+                      'custom-properties': {
+                        preserve: false,
+                      },
+                    },
+                  }),
+                  require('postcss-nested')(),
+                  require('postcss-hexrgba')(),
+                  require('postcss-calc')(),
+                  require('postcss-custom-media')(),
+                  require('cssnano')(),
+                ],
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(gif|jpe?g|png)$/,
+          loader: 'url-loader',
+          options: {
+            limit: 25000,
+            name: '[hash:7].[ext]',
+          },
+        },
+        {
+          test: /\.(ttf|otf|eot|svg|woff(2)?)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[hash:7].[ext]',
+          },
+        },
       ],
     },
     devServer: {
