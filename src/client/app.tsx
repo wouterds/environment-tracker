c0df283d2dc2d1227799b06ee382890168abbbb8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-27 17:25:02 +0100

    :zap: Just import render method

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import POC from './components/POC';
 import './styles/core.css';
 
-ReactDOM.render(<POC />, document.getElementById('root'));
+render(<POC />, document.getElementById('root'));
