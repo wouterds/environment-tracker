94df7c24a8aeca61251afc27be1f576d8c1393be
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-27 17:26:34 +0100

    :sparkles: Wrap root component with store provider

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import POC from './components/POC';
+import store from './store';
 import './styles/core.css';
 
-render(<POC />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <POC />
+  </Provider>,
+  document.getElementById('root'),
+);
