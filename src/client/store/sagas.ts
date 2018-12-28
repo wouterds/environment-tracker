e7b874ed969cf52cf394be99ef54a9617dfc401e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-28 12:46:05 +0100

    :sparkles: Link all sagas

--- src/client/store/sagas.ts
+++ src/client/store/sagas.ts
@@ -1,6 +1,8 @@
 import { all } from 'redux-saga/effects';
+import samples from './samples/sagas';
 import sensors from './sensors/sagas';
+import timeframes from './timeframe/sagas';
 
 export default function*() {
-  yield all([...sensors]);
+  yield all([...samples, ...sensors, ...timeframes]);
 }
