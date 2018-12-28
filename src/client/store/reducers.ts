c7c13bc2c95203f16b6eca5895d0bfcf0ebb31f3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-28 11:24:38 +0100

    :sparkles: Add reducers

--- src/client/store/reducers.ts
+++ src/client/store/reducers.ts
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import samples from './samples/reducer';
+import sensors from './sensors/reducer';
 
-export default combineReducers({});
+export default combineReducers({
+  sensors,
+  samples,
+});
