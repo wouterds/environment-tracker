16dda25f5e1914fa52e7ae36bd47cff7d761523e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-28 18:07:21 +0100

    :sparkles: Add timeframe to reducers

--- src/client/store/reducers.ts
+++ src/client/store/reducers.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import samples from './samples/reducer';
 import sensors from './sensors/reducer';
+import timeframe from './timeframe/reducer';
 
 export default combineReducers({
   sensors,
   samples,
+  timeframe,
 });
