5970b120f70c79b956ac58a53dc02a43121b97f0
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-28 18:48:19 +0100

    :sparkles: Pass timeframe & samples too

--- src/client/components/App/container.tsx
+++ src/client/components/App/container.tsx
@@ -1,12 +1,19 @@
+import { find } from 'lodash';
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { getSamples } from '../../store/samples/selectors';
+import { Sample } from '../../store/samples/types';
 import { fetch as fetchSensors } from '../../store/sensors/actions';
 import { getSensors } from '../../store/sensors/selectors';
+import { Sensor, Type } from '../../store/sensors/types';
 import { TIMEFRAMES } from '../../store/timeframe/config';
+import { getTimeframe } from '../../store/timeframe/selectors';
 
 interface Props {
   fetchSensors: () => void;
+  sensors: Sensor[];
+  samples: Sample[];
 }
 
 const wrapApp = (WrappedComponent: any) => {
@@ -16,9 +23,38 @@ const wrapApp = (WrappedComponent: any) => {
     }
 
     public render() {
+      const { sensors, samples } = this.props;
+
+      const illuminanceSensor = find(sensors, { type: Type.ILLUMINANCE });
+      const humiditySensor = find(sensors, { type: Type.HUMIDITY });
+      const temperatureSensor = find(sensors, { type: Type.TEMPERATURE });
+      const pressureSensor = find(sensors, { type: Type.PRESSURE });
+      const eco2Sensor = find(sensors, { type: Type.ECO2 });
+
       const props = {
         ...this.props,
         timeframes: TIMEFRAMES,
+        samples: {
+          [Type.ILLUMINANCE]:
+            (illuminanceSensor
+              ? find(samples, { sensorId: illuminanceSensor.id })
+              : []) || [],
+          [Type.HUMIDITY]:
+            (humiditySensor
+              ? find(samples, { sensorId: humiditySensor.id })
+              : []) || [],
+          [Type.TEMPERATURE]:
+            (temperatureSensor
+              ? find(samples, { sensorId: temperatureSensor.id })
+              : []) || [],
+          [Type.PRESSURE]:
+            (pressureSensor
+              ? find(samples, { sensorId: pressureSensor.id })
+              : []) || [],
+          [Type.ECO2]:
+            (eco2Sensor ? find(samples, { sensorId: eco2Sensor.id }) : []) ||
+            [],
+        },
       };
 
       return <WrappedComponent {...props} />;
@@ -29,10 +65,13 @@ const wrapApp = (WrappedComponent: any) => {
     };
   }
 
-  const mapStateToProps = (state: any) =>
-    createStructuredSelector({
+  const mapStateToProps = (state: any) => {
+    return createStructuredSelector({
       sensors: getSensors,
+      samples: getSamples,
+      timeframe: getTimeframe,
     })(state);
+  };
 
   const mapDispatchToProps = {
     fetchSensors,
--- src/client/components/App/index.tsx
+++ src/client/components/App/index.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
-import { Sensor } from '../../store/sensors/types';
+import { Sample } from '../../store/samples/types';
+import { Sensor, Type } from '../../store/sensors/types';
 import withContainer from './container';
 
 interface Props {
+  timeframe: number;
   timeframes: number[];
   sensors: Sensor[];
+  samples: {
+    [Type.ILLUMINANCE]: Sample[];
+    [Type.HUMIDITY]: Sample[];
+    [Type.TEMPERATURE]: Sample[];
+    [Type.PRESSURE]: Sample[];
+    [Type.ECO2]: Sample[];
+  };
 }
 
 class App extends React.Component<Props> {
