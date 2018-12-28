71e715f81dabe111823b740f82826b5a806100bd
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2018-12-28 11:22:07 +0100

    :sparkles: Sample sagas

---
+++ src/client/store/samples/sagas.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosResponse } from 'axios';
+import { call, fork, put, select, takeEvery } from 'redux-saga/effects';
+import { API_ENDPOINT } from '../../config';
+import { getSensors } from '../sensors/selectors';
+import { Sensor } from '../sensors/types';
+import { FETCH, fetchSuccess } from './actions';
+import { Sample } from './types';
+
+function* fetchFlow() {
+  const sensors: Sensor[] = yield select(getSensors);
+
+  let samples: Sample[] = [];
+  for (const sensor of sensors) {
+    try {
+      const response: AxiosResponse = yield call(
+        axios.get,
+        `${API_ENDPOINT}/samples`,
+        {
+          params: {
+            sensorId: sensor.id,
+          },
+        },
+      );
+
+      if (response.status !== 200) {
+        continue;
+      }
+
+      samples = [...samples, response.data];
+
+      yield put(fetchSuccess(samples));
+    } catch (error) {
+      // yield put(fetchError());
+      continue;
+    }
+  }
+}
+
+function* fetchFlowWatcher() {
+  yield takeEvery(FETCH, fetchFlow);
+}
+
+export default [fork(fetchFlowWatcher)];
