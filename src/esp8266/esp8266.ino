6258d2f70ce3cde808b950a16dd2532aa2a82277
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-01-09 12:25:44 +0100

    :art: Clean up getAbsoluteHumidity

--- src/esp8266/esp8266.ino
+++ src/esp8266/esp8266.ino
@@ -75,14 +75,17 @@ void setupLed()
 
 /*
  * @param float temperature [°C]
- * @param float humidity [%RH]
- * @return uint32_t absoluteHumidityScaled [mg/m^3]
+ * @param float relativeHumidity [%RH]
+ * @return int absoluteHumidityScaled [mg/m^3]
  */
-uint32_t getAbsoluteHumidity(float temperature, float humidity) {
-  // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
-  const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
-  const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
-  return absoluteHumidityScaled;
+int getAbsoluteHumidity(float temperature, float relativeHumidity) {
+  const float absoluteHumidity = 216.7f * (
+    (relativeHumidity / 100.0f) * 6.112f * exp(
+      (17.62f * temperature) / (243.12f + temperature)
+    ) / (273.15f + temperature)
+  ); // [g/m^3]
+
+  return static_cast<int>(1000.0f * absoluteHumidity); // [mg/m^3]
 }
 
 void setup(void)
