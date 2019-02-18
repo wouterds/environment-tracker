7beff9c2069ef4ba8cc8c07ffc3b6b4a63b11569
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-02-18 08:53:15 +0100

    :zap: Use new sensors endpoint

--- src/server/scripts/take-sample.ts
+++ src/server/scripts/take-sample.ts
@@ -25,7 +25,7 @@ interface ApiResponse {
   const samples: ApiResponse[] = [];
   while (isFuture(endTime)) {
     try {
-      const response = await axios.get('http://esp8266');
+      const response = await axios.get('http://esp8266-sensors');
 
       if (response.status !== 200) {
         continue;
