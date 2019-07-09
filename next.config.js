10767e39f40ba34bf2f690952ae624202ccd0476
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-07-09 22:30:45 +0200

    :wrench: Configure dotenv

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const typescript = require('@zeit/next-typescript');
+const dotenv = require('dotenv-webpack');
 
 const config = {
   distDir: '../dist/app',
@@ -7,4 +9,19 @@ const config = {
 
 module.exports = withPlugins([
   [typescript],
-], config);
+], {
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+      new dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    ];
+
+    return config;
+  },
+  ...config,
+});
