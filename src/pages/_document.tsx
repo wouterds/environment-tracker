74544aabca683c48ed8fe18adf4dc774e9e459e9
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-07-09 21:33:28 +0200

    :wrench: SSR styled compoments

---
+++ src/pages/_document.tsx
@@ -0,0 +1,31 @@
+import NextDocument from 'next/document';
+import { ReactType } from 'react';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class Document extends NextDocument {
+  public static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: ReactType) => (props: object) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await NextDocument.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
