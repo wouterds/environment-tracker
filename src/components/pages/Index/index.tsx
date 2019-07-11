0055b4a7ac493b95f3735e099ce97328ab9141ac
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-07-11 14:09:12 +0200

    :construction: Testing chart

--- src/components/pages/Index/index.tsx
+++ src/components/pages/Index/index.tsx
@@ -1,7 +1,44 @@
+import axios from 'axios';
 import Layout from 'components/Layout';
+import { useState } from 'react';
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
-export default () => (
-  <Layout>
-    <h1>Hello World</h1>
-  </Layout>
-);
+export default () => {
+  const [data, setData] = useState([]);
+
+  try {
+    (async () => {
+      const { data: response } = await axios.get(
+        '/api/measurements?sensor=temperature&groupByMinutes=5',
+      );
+
+      setData(response);
+    })().catch();
+  } catch (e) {
+    // silent catch error
+  }
+
+  return (
+    <Layout>
+      <AreaChart width={500} height={400} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="dtime" />
+        <YAxis />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="average"
+          stroke="#8884d8"
+          fill="#8884d8"
+        />
+      </AreaChart>
+    </Layout>
+  );
+};
