f9dc91b8137ede021851d0abc8710ddec1838bcf
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-07-11 22:57:09 +0200

    :sparkles: Average, low & high

--- src/components/pages/Index/index.tsx
+++ src/components/pages/Index/index.tsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import Layout from 'components/Layout';
+import maxBy from 'lodash/maxBy';
+import meanBy from 'lodash/meanBy';
+import minBy from 'lodash/minBy';
 import { useEffect, useState } from 'react';
 import { Line, LineChart, ResponsiveContainer, Tooltip, YAxis } from 'recharts';
-import { Chart, Container } from './styles';
+import { Chart, ChartContent, ChartFooter, Container } from './styles';
 import CustomTooltip from './Tooltip';
 
 export default () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Array<{ average: number }>>([]);
 
   useEffect(() => {
     try {
@@ -22,33 +25,53 @@ export default () => {
     }
   }, [true]);
 
+  const high = maxBy(data, 'average');
+  const average = meanBy(data, 'average');
+  const low = minBy(data, 'average');
+
   return (
     <Layout>
       <Container>
         <Chart>
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              width={768}
-              height={300}
-              data={data}
-              margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
-            >
-              <Line
-                type="monotone"
-                dataKey="average"
-                stroke="#e74c3c"
-                strokeWidth={2}
-                dot={false}
-              />
-              <YAxis domain={['auto', 'auto']} hide={true} />
-              <Tooltip
-                cursor={false}
-                content={(props: any) => (
-                  <CustomTooltip {...props} unit={'°C'} name={name} />
-                )}
-              />
-            </LineChart>
-          </ResponsiveContainer>
+          <ChartContent>
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                width={768}
+                height={300}
+                data={data}
+                margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
+              >
+                <Line
+                  type="monotone"
+                  dataKey="average"
+                  stroke="#e74c3c"
+                  strokeWidth={2}
+                  dot={false}
+                />
+                <YAxis domain={['auto', 'auto']} hide={true} />
+                <Tooltip
+                  cursor={false}
+                  content={(props: any) => (
+                    <CustomTooltip {...props} unit={'°C'} name={name} />
+                  )}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </ChartContent>
+          <ChartFooter>
+            <li>
+              <label>High</label>
+              <span>{high ? high.average.toFixed(2) : ''} °C</span>
+            </li>
+            <li>
+              <label>Average</label>
+              <span>{average ? average.toFixed(2) : ''} °C</span>
+            </li>
+            <li>
+              <label>Low</label>
+              <span>{low ? low.average.toFixed(2) : ''} °C</span>
+            </li>
+          </ChartFooter>
         </Chart>
       </Container>
     </Layout>
