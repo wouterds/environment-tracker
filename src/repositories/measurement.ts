acfc9eb88013b9f2143bf0cace17a343be98ea13
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-07-11 13:45:09 +0200

    :card_file_box: Added getBySensorGroupedPerMinutes

--- src/repositories/measurement.ts
+++ src/repositories/measurement.ts
@@ -1,4 +1,6 @@
 import Measurement, { Definition } from 'models/measurement';
+import { QueryTypes } from 'sequelize';
+import db from 'services/database';
 
 const add = async (data: {
   sensor: string;
@@ -9,6 +11,33 @@ const add = async (data: {
   return measurement.get({ plain: true });
 };
 
+const getBySensorGroupedPerMinutes = async (
+  sensor: string,
+  groupedPerMinutes: number,
+): Promise<Array<{ average: number; dtime: string }>> => {
+  const rows = await db.query(
+    `
+      SELECT
+      AVG(\`value\`) AS \`average\`,
+      CONVERT((MIN(\`createdAt\`) DIV :groupedPerMinutes) * :groupedPerMinutes, DATETIME) AS \`dtime\`
+      FROM \`Measurements\`
+      WHERE \`sensor\` = :sensor
+      GROUP BY \`createdAt\` DIV :groupedPerMinutes
+  `,
+    {
+      replacements: {
+        sensor,
+        groupedPerMinutes: groupedPerMinutes * 100,
+      },
+      type: QueryTypes.SELECT,
+      raw: true,
+    },
+  );
+
+  return rows;
+};
+
 export default {
   add,
+  getBySensorGroupedPerMinutes,
 };
