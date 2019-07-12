0ae00f279e4296f2e294a57a6b85b3c23849e212
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-07-12 13:34:16 +0200

    :fire: Removed dead code

--- src/components/pages/Index/Chart/index.tsx
+++ src/components/pages/Index/Chart/index.tsx
@@ -68,13 +68,7 @@ export default (props: Props) => {
           </LastValue>
         )}
         <ResponsiveContainer width="100%" height="100%">
-          <LineChart
-            width={768}
-            height={300}
-            data={data}
-            margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
-            syncId={syncId}
-          >
+          <LineChart data={data} margin={{ left: 0, right: 0 }} syncId={syncId}>
             <Line
               type="monotone"
               dataKey="value"
